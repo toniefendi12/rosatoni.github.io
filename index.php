39bfd67123e5e238e4358f17e95bbea58d711aa7
_toniefendi <tontonefendi12@gmail.com>
2021-08-13 17:11:23 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All About Rosa & Toni</title>
+</head>
+<body>
+	<div style="width: 100%; margin-right: auto; margin-left: auto;">
+		<h3>Stay tune for <label style="color: red;">Agustus, 26</label></h3>
+		<h5>Emilia Rosa & Toni Efendi</h5>
+	</div>
+</body>
+</html>
